3d0b97b9fe5b4cd527ee895ef1927daa2a87df7d
Tyler Walker <twalker1998@ou.edu>
2019-05-08 19:37:39 -0500

    created search service

--- obis-search-test/src/app/core/search/search.service.ts
+++ obis-search-test/src/app/core/search/search.service.ts
@@ -1,9 +1,83 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ApiService } from '../api/api.service'
+
+import { Api_Response } from '../../models/api_response';
+import { Acctax } from '../../models/acctax';
+import { Comtax } from '../../models/comtax';
+import { Syntax } from '../../models/syntax';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private response: Api_Response;
+  private results: Array<Acctax | Comtax | Syntax> = [];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private apiService: ApiService) { }
+
+  get_results() {
+    return this.results;
+  }
+
+  search(query: string): void {
+    this.apiService.get_query("acctax", "sname", query).subscribe((response: Api_Response) => {
+      this.response = response;
+
+      this.parse_response(this.response, 0, "acctax");
+
+      this.apiService.get_query("acctax", "genus", query).subscribe((response: Api_Response) => {
+        this.response = response;
+
+        this.parse_response(this.response, 0, "acctax");
+
+        this.apiService.get_query("acctax", "species", query).subscribe((response: Api_Response) => {
+          this.response = response;
+
+          this.parse_response(this.response, 0, "acctax");
+
+          this.apiService.get_query("syntax", "sname", query).subscribe((response: Api_Response) => {
+            this.response = response;
+
+            this.parse_response(this.response, 0, "syntax");
+
+            this.apiService.get_query("comtax", "vernacularname", query).subscribe((response: Api_Response) => {
+              this.response = response;
+
+              this.parse_response(this.response, 0, "comtax");
+            });
+          });
+        });
+      });
+    });
+  }
+
+  parse_response(response: Api_Response, count: number, type: string) {
+    var new_count = (response.count - response.results.length) - count;
+    var next_url = response.next;
+
+    for(let result of response.results) {
+      if(type == "acctax") {
+        result = <Acctax>result;
+      } else if(type == "comtax") {
+        result = <Comtax>result;
+      } else if(type == "syntax") {
+        result = <Syntax>result;
+      }
+
+      this.results.push(result);
+      count++;
+    }
+
+    if(new_count > 0) {
+      next_url = next_url.replace("http", "https");
+
+      this.apiService.get_url(next_url).subscribe((response: Api_Response) => {
+        this.response = response;
+
+        this.parse_response(this.response, count, "acctax");
+      });
+    }
+  }
 }
