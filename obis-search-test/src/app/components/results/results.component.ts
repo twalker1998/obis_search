ca0325d56e829079fad3ee0260cb035ee07749c0
Tyler Walker <twalker1998@ou.edu>
2019-05-09 12:23:50 -0500

    added code to update results componen

--- obis-search-test/src/app/components/results/results.component.ts
+++ obis-search-test/src/app/components/results/results.component.ts
@@ -4,6 +4,8 @@ import { Acctax } from '../../models/acctax';
 import { Comtax } from '../../models/comtax';
 import { Syntax } from '../../models/syntax';
 
+import { ResultsService } from '../../core/results/results.service';
+
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
@@ -11,8 +13,13 @@ import { Syntax } from '../../models/syntax';
 })
 export class ResultsComponent {
   private results: Set<Acctax | Comtax | Syntax>;
+  isQueryComplete: boolean;
 
-  constructor() { }
+  constructor(private resultsService: ResultsService) {
+    this.resultsService.isQueryComplete.subscribe(value => {
+      this.isQueryComplete = value;
+    });
+  }
 
   render_results(results: Set<Acctax | Comtax | Syntax>) {
     this.results = results;
