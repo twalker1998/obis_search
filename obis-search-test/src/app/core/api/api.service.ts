186a20a480841515ba20dc10569b735f663bcb12
Tyler Walker <twalker1998@ou.edu>
2019-05-16 17:15:23 -0500

    added error checking to api service

--- obis-search-test/src/app/core/api/api.service.ts
+++ obis-search-test/src/app/core/api/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 import { Api_Response } from '../../models/api_response';
 import { Acctax } from '../../models/acctax';
@@ -17,30 +19,61 @@ export class ApiService {
   constructor(private httpClient : HttpClient) { }
 
   get_acctax(url: string) {
-    return this.httpClient.get<Acctax>(url);
+    return this.httpClient.get<Acctax>(url).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   get_hightax(url: string) {
-    return this.httpClient.get<Hightax>(url);
+    return this.httpClient.get<Hightax>(url).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   get_swap(url: string) {
-    return this.httpClient.get<Swap>(url);
+    return this.httpClient.get<Swap>(url).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   get_fedstatus(url: string) {
-    return this.httpClient.get<FedStatus>(url);
+    return this.httpClient.get<FedStatus>(url).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   get_ststatus(url: string) {
-    return this.httpClient.get<StateStatus>(url);
+    return this.httpClient.get<StateStatus>(url).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   get_url(url: string) {
-    return this.httpClient.get<Api_Response>(url);
+    return this.httpClient.get<Api_Response>(url).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   get_query(table: string, field: string, query: string) {
-    return this.httpClient.get<Api_Response>(this.baseUrl + table + '/?' + field + '=' + query + '&format=json');
+    return this.httpClient.get<Api_Response>(this.baseUrl + table + '/?' + field + '=' + query + '&format=json').pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if(error.error instanceof ErrorEvent) {
+      console.error("An error occurred: ", error.error.message);
+    } else {
+      console.error("Backend returned code ${error.status}, body was: ${error.error}");
+    }
+
+    return throwError("Something bad happened, please try again later.");
   }
 }
