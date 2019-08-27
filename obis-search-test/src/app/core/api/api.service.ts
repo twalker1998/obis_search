62ebfd48dc0bc3819e2c70aa8922c2356fee31eb
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2019-08-27 16:16:39 -0500

    Removed promises from get_hightax and get_acctax in api service

--- obis-search-test/src/app/core/api/api.service.ts
+++ obis-search-test/src/app/core/api/api.service.ts
@@ -22,14 +22,14 @@ export class ApiService {
     return this.httpClient.get<Acctax>(url).pipe(
       retry(1),
       catchError(this.handleError)
-    ).toPromise();
+    );
   }
 
   get_hightax(url: string) {
     return this.httpClient.get<Hightax>(url).pipe(
       retry(1),
       catchError(this.handleError)
-    ).toPromise();
+    );
   }
 
   get_swap(url: string) {
