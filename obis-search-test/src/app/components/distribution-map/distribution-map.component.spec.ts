1e496bc433aff7fb426fbcb4e60804d0e96b3622
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2019-08-29 16:51:49 -0500

    Updated spec file for distribution map

--- obis-search-test/src/app/components/distribution-map/distribution-map.component.spec.ts
+++ obis-search-test/src/app/components/distribution-map/distribution-map.component.spec.ts
@@ -5,21 +5,27 @@ import { DistributionMapComponent } from './distribution-map.component';
 describe('DistributionMapComponent', () => {
   let component: DistributionMapComponent;
   let fixture: ComponentFixture<DistributionMapComponent>;
+  let app: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DistributionMapComponent ]
     })
     .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(DistributionMapComponent);
+    app = fixture.debugElement.componentInstance;
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('verify default values', () => {
+    expect(app.basemap).toEqual(jasmine.any(String));
+    expect(app.center).toEqual(jasmine.any(Array));
+    expect(app.zoom).toEqual(jasmine.any(Number));
+    expect(app.mapLoaded).toEqual(false);
   });
 });
