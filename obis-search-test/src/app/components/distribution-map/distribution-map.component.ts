447c4b691077960c7540547ea11a1f59243c8582
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2019-08-29 16:54:48 -0500

    Added code to display map in component ts file

--- obis-search-test/src/app/components/distribution-map/distribution-map.component.ts
+++ obis-search-test/src/app/components/distribution-map/distribution-map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, Output, EventEmitter } from '@angular/core';
+import { loadModules } from 'esri-loader';
+import esri = __esri;
 
 @Component({
   selector: 'app-distribution-map',
@@ -7,9 +9,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DistributionMapComponent implements OnInit {
 
+  @Output() mapLoadedEvent = new EventEmitter<boolean>();
+
+  // The <div> where we will place the map
+  @ViewChild('mapViewNode', {static: true}) private mapViewEl: ElementRef;
+
+  /**
+   * _zoom sets map zoom
+   * _center sets map center
+   * _basemap sets type of map
+   * _loaded provides map loaded status
+   */
+  private _zoom = 10;
+  private _center: Array<number> = [0.1278, 51.5074];
+  private _basemap = 'streets';
+  private _loaded = false;
+
+  get mapLoaded(): boolean {
+    return this._loaded;
+  }
+
+  @Input()
+  set zoom(zoom: number) {
+    this._zoom = zoom;
+  }
+
+  get zoom(): number {
+    return this._zoom;
+  }
+
+  @Input()
+  set center(center: Array<number>) {
+    this._center = center;
+  }
+
+  get center(): Array<number> {
+    return this._center;
+  }
+
+  @Input()
+  set basemap(basemap: string) {
+    this._basemap = basemap;
+  }
+
+  get basemap(): string {
+    return this._basemap;
+  }
+
   constructor() { }
 
+  async initializeMap() {
+    try {
+
+      // Load the modules for the ArcGIS API for JavaScript
+      const [EsriMap, EsriMapView] = await loadModules([
+        'esri/Map',
+        'esri/views/MapView'
+      ]);
+
+      // Configure the Map
+      const mapProperties: esri.MapProperties = {
+        basemap: this._basemap
+      };
+
+      const map: esri.Map = new EsriMap(mapProperties);
+
+      // Initialize the MapView
+      const mapViewProperties: esri.MapViewProperties = {
+        container: this.mapViewEl.nativeElement,
+        center: this._center,
+        zoom: this._zoom,
+        map: map
+      };
+
+      return new EsriMapView(mapViewProperties);
+
+    } catch (error) {
+      console.log('EsriLoader: ', error);
+    }
+
+  }
+
+  // Finalize a few things once the MapView has been loaded
+  houseKeeping(mapView) {
+    mapView.when(() => {
+      console.log('mapView ready: ', mapView.ready);
+      this._loaded = mapView.ready;
+      this.mapLoadedEvent.emit(true);
+    });
+  }
+
   ngOnInit() {
+    // Initialize MapView and return an instance of MapView
+    this.initializeMap().then((mapView) => {
+      this.houseKeeping(mapView);
+    });
   }
 
 }
