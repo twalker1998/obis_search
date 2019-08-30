3069a645a07db705ce213ebe8ef78131808ef9ac
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2019-08-30 13:22:26 -0500

    Removed unnecessary inputs in dist map comp

--- obis-search-test/src/app/components/distribution-map/distribution-map.component.ts
+++ obis-search-test/src/app/components/distribution-map/distribution-map.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { loadModules } from 'esri-loader';
-import esri = __esri;
 
 @Component({
   selector: 'app-distribution-map',
