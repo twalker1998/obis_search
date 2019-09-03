4345677a33513eb2cc9bb33219631390cc843ce4
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2019-09-03 12:49:49 -0500

    Added function to download file

--- obis-search-test/src/app/components/result/result.component.ts
+++ obis-search-test/src/app/components/result/result.component.ts
@@ -164,6 +164,19 @@ export class ResultComponent implements OnInit {
     });
   }
 
+  download(filename: string, text: any) {
+    var element = document.createElement("a");
+    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+    element.setAttribute("download", filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
   generate_csv() {
     let filename = this.result.sname;
     let firstRow = '"County", "Count"';
