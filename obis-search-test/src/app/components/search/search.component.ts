aa34b68d7b0988e74c75a53fccd37ebb39274ed1
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2019-09-18 14:53:17 -0500

    Added function to get cookies

--- obis-search-test/src/app/components/search/search.component.ts
+++ obis-search-test/src/app/components/search/search.component.ts
@@ -64,4 +64,23 @@ export class SearchComponent {
   createCookie(name: string, value: string): void {
     document.cookie = name + "=" + value + "; path=/";
   }
+
+  getCookie(name: string): string {
+    if(document.cookie.length > 0) {
+      let start = document.cookie.indexOf(name + "=");
+
+      if(start != -1) {
+        start = start + name.length + 1;
+        let end = document.cookie.indexOf(";", start);
+
+        if(end == -1) {
+          end = document.cookie.length;
+        }
+
+        return unescape(document.cookie.substring(start, end));
+      }
+    }
+
+    return "";
+  }
 }
