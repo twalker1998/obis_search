caad5afc6b16c25f61d31209bbbbf9e14a14b64b
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2019-11-21 13:36:25 -0600

    Removed unnecessary code from app component ts

--- obis-search-test/src/app/app.component.ts
+++ obis-search-test/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { MapService } from './core/map/map.service';
-
-// import { startWith, tap, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +7,6 @@ import { MapService } from './core/map/map.service';
 })
 export class AppComponent {
   title = 'OBIS Search';
-  isResultLoaded: boolean;
 
-  constructor(private mapService: MapService) {
-    this.mapService.isResultLoaded.subscribe(value => {
-      this.isResultLoaded = value;
-    });
-  }
-
-  // ngAfterViewInit() {
-  //   this.mapService.isResultLoaded.pipe(
-  //     startWith(null),
-  //     delay(0),
-  //     tap(() => this.mapService.isResultLoaded.subscribe(value => {
-  //       this.isResultLoaded = value
-  //     }))
-  //   ).subscribe();
-  // }
+  constructor() { }
 }
