ebf88246fdd9403382701fc70f9f11ababb6b321
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2019-11-21 13:32:59 -0600

    Added acode field to Map service

--- obis-search-test/src/app/core/map/map.service.ts
+++ obis-search-test/src/app/core/map/map.service.ts
@@ -6,6 +6,12 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class MapService {
   public isResultLoaded: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private acodeSource: BehaviorSubject<string> = new BehaviorSubject<string>("search");
+  acode = this.acodeSource.asObservable();
 
   constructor() { }
+
+  changeAcode(acode: string) {
+    this.acodeSource.next(acode);
+  }
 }
