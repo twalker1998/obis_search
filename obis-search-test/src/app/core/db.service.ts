b48eb3e9e7212ee53756477ae5619d054bb3215f
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-02-10 10:38:29 -0600

    Created db service

---
+++ obis-search-test/src/app/core/db.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { OccurrenceData } from '../models/occurrence_data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbService {
+  baseUrl = 'http://obsvweb1.ou.edu/obis_db_scripts/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOccurrenceData(sname: string) {
+    return this.httpClient.get<OccurrenceData>(this.baseUrl + 'occurrence-table.php?sname=' + sname);
+  }
+}
