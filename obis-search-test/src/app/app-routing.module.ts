4eace24bf3502cb430668c7610baeb624ab62f64
Tyler Walker <twalker@Tylers-MacBook-Pro.local>
2020-02-10 10:50:09 -0600

    Organized imports and fixed warning in app routing module

--- obis-search-test/src/app/app-routing.module.ts
+++ obis-search-test/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SearchComponent } from './components/search/search.component';
 import { ResultComponent } from './components/result/result.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
-  { path: "result/:acode", component: ResultComponent },
-  { path: "./", component: SearchComponent }
+  { path: 'result/:acode', component: ResultComponent },
+  { path: './', component: SearchComponent }
 ];
 
 @NgModule({
